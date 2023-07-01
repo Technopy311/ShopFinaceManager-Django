c7068a4a573a70a8ad0cc154f6750759652f0105
Technopy311 <xmenmagnet@gmail.com>
2023-07-01 20:25:38 +0000

    chore: Established Media & Static paths

--- ManagerProject/ManagerProject/settings.py
+++ ManagerProject/ManagerProject/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATIC_ROOT = '/home/technopy/ShopFinaceManager-Django/allstatic'
+MEDIA_ROOT = '/home/technopy/ShopFinaceManager-Django/allmedia'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
