8afffa54806e78443436c26ee94b27488b0ea4eb
Technopy311 <xmenmagnet@gmail.com>
2023-07-05 03:35:05 +0000

    chore: add Orders & Payments dynamic list views

--- ManagerProject/core/admin.py
+++ ManagerProject/core/admin.py
@@ -3,6 +3,39 @@ from . import models as core_models
 
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = [
+        ("Edit Product",
+         {"fields": ["name", "price", "stock", "description", "image"]}
+        )
+    ]
+
+    list_filter = ["price", "stock", "name"]
+    list_display = ["name", "price", "stock"]
 
 admin.site.register(core_models.Product, ProductAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Edit Order",
+         {"fields": ["product"]}
+        )
+    ]
+
+    list_filter = ["product", "updated_datetime", "created_datetime"]
+    list_display = ["product", "updated_datetime", "created_datetime"]
+
+admin.site.register(core_models.Order, OrderAdmin)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Edit Payment",
+         {"fields": ["order", "amount", "status", "method"]}
+        )
+    ]
+
+    list_filter = ["order", "status", "amount", "method"]
+    list_display = ["order", "amount", "status", "method"]
+
+admin.site.register(core_models.Payment, PaymentAdmin)
--- ManagerProject/core/models.py
+++ ManagerProject/core/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     image = models.ImageField("Product Image", default=None)
 
     def __str__(self):
-        return f"{self.name} -- {self.price} -- {self.stock}"
+        return f"{self.name}"
 
 
 class Order(models.Model):
@@ -21,10 +21,13 @@ class Order(models.Model):
         on_delete=models.CASCADE,
     )
 
+    def __str__(self):
+        return f"{self.product}"
+
 
 class Payment(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    amount = models.IntegerField("Cantidad de Pago", default=0)
+    amount = models.IntegerField("Payment amount", default=0)
     status = models.BooleanField("Paid", default=False)
     # Payment Methods
     DEBIT = "DEBI"
@@ -40,3 +43,7 @@ class Payment(models.Model):
     ]
 
     method = models.CharField(choices=payment_method_choices, default="", max_length=40)
+
+
+    def __str__(self):
+        return f"{self.amount} -- {self.status}"
