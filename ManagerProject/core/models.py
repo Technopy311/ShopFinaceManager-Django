8355dff1003f25f68bb0c4e80f8aefd78d4488e2
Technopy311 <xmenmagnet@gmail.com>
2023-07-13 06:49:05 +0000

    feat (core Models): update Transaction models related to a product when this last one is saved with any change price.

--- ManagerProject/core/models.py
+++ ManagerProject/core/models.py
@@ -1,16 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-
-
-class Order(models.Model):
-    ordered_by = models.CharField("Person who ordered", max_length=100, default="---")
-    created_datetime = models.DateTimeField("Created datetime", auto_now_add=True)
-    updated_datetime = models.DateTimeField("Updated datetime", auto_now=True)
-    completed = models.BooleanField("Order Status", default=False)
-
-
-    def __str__(self):
-        return f"Order #{self.pk}"
 
 
 class Product(models.Model):
@@ -21,12 +12,44 @@ class Product(models.Model):
     description = models.TextField("Product Description", default="", max_length=300)
     image = models.ImageField("Product Image", default=None)
 
-    order = models.ForeignKey(Order, verbose_name="Corresponding Order", default=None, on_delete=models.CASCADE)
 
 
     def __str__(self):
         return f"{self.name}"
 
+
+# Signal to update transactions when product's prices change
+@receiver(post_save, sender=Product)
+def product_changed_callback(sender, instance, **kwargs):
+    
+    prev_obj = Product.objects.get(pk=instance.pk)  # Before save
+    
+    # Check if some price has changed
+
+    if (prev_obj.price_cost is not instance.price_cost) or (prev_obj.price_sell is not instance.price_sell):
+        
+        # Access each produdcts_order and then re-save each transaction so it is updated
+        related_orders = instance.order_set.all()
+        
+        for order in related_orders:
+            print(order.transaction)
+            order.transaction.save()
+    else:
+        pass
+
+
+
+
+class Order(models.Model):
+    ordered_by = models.CharField("Person who ordered", max_length=100, default="---")
+    created_datetime = models.DateTimeField("Created datetime", auto_now_add=True)
+    updated_datetime = models.DateTimeField("Updated datetime", auto_now=True)
+    completed = models.BooleanField("Order Status", default=False)
+
+    products = models.ManyToManyField(Product, verbose_name="Order's Products", default=None)
+
+    def __str__(self):
+        return f"Order #{self.pk}"
 
 
 class Payment(models.Model):
